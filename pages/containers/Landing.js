81d82e6a0061848b8e71457f3d1a1af6e706cd36
ImagineRabbits <mixxdarkz2@gmail.com>
2017-09-10 02:50:32 +0700

    landing page

--- pages/containers/Landing.js
+++ pages/containers/Landing.js
@@ -1,20 +1,35 @@
 import React from 'react'
 import styled from 'styled-components'
 
+const Register = styled.button`
+  border: 5px solid #fff;
+  font-size: 2em;
+  border-radius: 30px;
+  padding: 0em 1em;
+  background: none;
+  color: #fff;
+  min-width: 270px;
+  margin: 0 auto;
+  cursor: pointer;
+  transition: 0.2s;
+
+  &:hover {
+    color: #000;
+    background-color: #fff;
+  }
+`
+
 class LandingPage extends React.Component {
   render() {
     return (
       <div className="row landing-height">
-        <div className="col-12 text-center">
-          <img src="../../static/img/logo-01.png" alt="logo-alpaca" />
+        <div className="hello-field col-12 text-center">
+          <img className="helloworld" src="../../static/img/logo.png" alt="logo-alpaca" />
         </div>
         <div className="col-12 text-center">
-          <h2>Alpaca</h2>
-          <h3>
-            Break Your Limit <br />
-            And Practice Your Skill <br />
-          </h3>
+          <img className="breaklimit" src="../../static/img/desc.png" alt="break-limit" />
         </div>
+        <Register style={{marginTop: '2em'}}>Register</Register>
       </div>
     )
   }
