10509099a484b90450e911cbe06c81d633700d38
ImagineRabbits <mixxdarkz2@gmail.com>
2017-09-10 17:51:58 +0700

    responsive

--- pages/containers/Team.js
+++ pages/containers/Team.js
@@ -2,9 +2,13 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Box = styled.div`
-  width: 100%;
-  height: 300px;
   margin: 1em 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 185px;
 `
 
 class TeamPage extends React.Component {
@@ -17,24 +21,28 @@ class TeamPage extends React.Component {
         <div className="col-12 text-center">
           <div className="container">
             <div className="row">
-              <div className="col-md-6">
+              <div className="col-md-3">
                 <Box>
                   <img className="team-logo-top" src="../../static/img/font.png" alt="" />
+                  Front-End
                 </Box>
               </div>
-              <div className="col-md-6">
+              <div className="col-md-3">
                 <Box>
                   <img className="team-logo-top" src="../../static/img/de.png" alt="" />
+                  Design
                 </Box>
               </div>
-              <div className="col-md-6">
+              <div className="col-md-3">
                 <Box>
-                  <img className="team-logo-bottom" src="../../static/img/game.png" alt="" />
+                  <img className="team-logo-top" src="../../static/img/game.png" alt="" />
+                  Game
                 </Box>
               </div>
-              <div className="col-md-6">
+              <div className="col-md-3">
                 <Box>
-                  <img className="team-logo-bottom" src="../../static/img/infra.png" alt="" />
+                  <img className="team-logo-top" src="../../static/img/infra.png" alt="" />
+                  Infrastructure
                 </Box>
               </div>
             </div>
--- pages/index.js
+++ pages/index.js
@@ -19,6 +19,7 @@ class Index extends React.Component {
       <div>
         <Head>
           <title>HelloWorld #Alpaca</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="stylesheet" href="/static/css/bootstrap.min.css" />
           <link rel="stylesheet" href="/static/css/style.css" />
           <script src="../static/js/jquery.min.js"></script>
