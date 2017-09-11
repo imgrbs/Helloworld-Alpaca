35cce6e3ab0bd023af57b15f4e226b4f78637782
ImagineRabbits <mixxdarkz2@gmail.com>
2017-09-12 01:59:44 +0700

    add alpaca gif contact

--- pages/containers/Contact.js
+++ pages/containers/Contact.js
@@ -2,43 +2,50 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Img = styled.img`
-  height: 215px;
+  height: 100%;
+  display: flex;
+`
+const Flur = Img.extend`
   position: absolute;
   bottom: 0;
-  display: flex;
-  `
-  const PicContainer = styled.div`
+  height: 25%; 
+  min-width: 1366px; 
+  width: 100%; 
+  z-index : 3;
+`
+
+const PicContainer = styled.div`
   width: 100%;
-  height: 40%;
-  left: 0;
-  bottom: 0;
+  height: 45%;
+  bottom: 2.5em;
   position: absolute;
   display: flex;
   justify-content: center;
-  flex-direction: row;
   align-items: center;
+  flex-direction: row;
 `
 
 class ContactPage extends React.Component {
   render() {
     return (
-      <div id="contact" className="row contact-height">
+      <div id="contact" className="row contact-height justify-content-center">
+        <Flur src="../../static/img/bg8-1.png" alt="alpaca-gif" />
         <div className="col-12 text-center">
           <img className="what-text" src="../../static/img/contact.png" alt="contact" />
-          {/* <PicContainer>
+          <PicContainer>
             <Img 
-              style={{ left: '20%' }} 
+              //style={{ bottom: '2.6em', left: '20%' }} 
             src="../../static/img/al1.gif" alt="alpaca-gif" />
             <Img 
-              style={{ left: '33%' }} 
+              //style={{ bottom: '2.6em', left: '35%' }} 
             src="../../static/img/al2.gif" alt="alpaca-gif" />
             <Img 
-              style={{ right: '33%' }} 
+              //style={{ bottom: '2.6em', right: '35%' }} 
             src="../../static/img/al3.gif" alt="alpaca-gif" />
             <Img 
-              style={{ right: '20%' }} 
+              //style={{ bottom: '2.6em', right: '20%' }} 
             src="../../static/img/al4.gif" alt="alpaca-gif" />
-          </PicContainer> */}
+          </PicContainer>
         </div>
       </div>
     )
