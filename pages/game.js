874ebd13cbe40bddce77ed700ebb3881f6cd6d8c
basinw <bas10600@gmail.com>
2017-09-13 11:11:51 +0700

    add <meta> for game page and edit title

--- pages/game.js
+++ pages/game.js
@@ -44,8 +44,20 @@ class GameContainer extends React.Component {
         return (
             <div>
                 <Head>
-                    <title>HelloWorld #Alpaca</title>
+                    <title>HelloWorld #Alpaca | game</title>
                     <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+                    <meta charset="UTF-8" />
+                    <meta property="og:title" content="HelloWorld #Alpaca" />
+                    <meta property="og:type" content="company" />
+                    <meta property="og:url" content="https://helloworld.itbangmod.in.th/" />
+                    <meta property="og:image" content="../static/img/og.png" />
+                    <meta property="og:site_name" content="HelloWorld #Alpaca By Alchemist" />
+                    <meta property="og:description" content="Break Your Limit and Practice Your Skill!" />
+                    <meta property="fb:page_id" content="1298270230190822" />
+                    <link rel="icon" type="image/ico" href="../static/favicon.ico" sizes="16x16" />
+                    <meta name="msapplication-TileColor" content="#FFD101" />
+                    <meta name="theme-color" content="#FFD101" />
 
                     <link rel="stylesheet" href="/static/css/bootstrap.min.css" />
                     <link rel="stylesheet" href="/static/css/style.css" />
